891aaf939add44b3a46ff96c8501a3f505ed76e7
paboludurga04 <paboludurga93@gmail.com>
2026-06-29 12:55:32 +0530

    Add CountDigits program to count number of digits

---
+++ CountDigits.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+
+        if (n==0){
+            System.out.println(1);
+            return;
+        }
+        n=Math.abs(n);
+        while (n>0){
+            count++;
+            n/=10;
+        }
+        System.out.println(count);
+    }
+}
